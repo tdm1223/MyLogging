2248bc52debb9408b35816d2a5f1280c56b3b17a
Jegyun Kang <tdm1223@naver.com>
2020-06-20 20:48:03 +0900

    [CREATE] Thread
    - Thread 클래스 생성

---
+++ MyLogging/MyLogging/Thread.cpp
@@ -0,0 +1,165 @@
+﻿#include "Thread.h"
+
+Thread::Thread()
+{
+}
+
+Thread::~Thread()
+{
+    ReleaseResource();
+}
+
+BOOL Thread::CreateThread(UINT32 waitTick)
+{
+    UINT32 uiThreadId = 0;
+    threadHandle_ = (HANDLE)_beginthreadex(NULL, 0, &Thread::CallTickThread, this, CREATE_SUSPENDED, &uiThreadId);
+    if (threadHandle_ == nullptr)
+    {
+        return FALSE;
+    }
+    waitTick_ = waitTick;
+
+    // 이벤트를 생성한다.
+    if (quitEvent == nullptr)
+    {
+        quitEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+    }
+    isRun_ = FALSE;
+    isQuit_ = FALSE;
+    return TRUE;
+}
+
+void Thread::TickThread()
+{
+    while (!isQuit_)
+    {
+        UINT32 dwRet = WaitForSingleObject(quitEvent, waitTick_);
+        if (WAIT_OBJECT_0 == dwRet)
+        {
+            if (threadHandle_)
+            {
+                CloseHandle(threadHandle_);
+                threadHandle_ = nullptr;
+            }
+            break;
+        }
+        else if (dwRet == WAIT_TIMEOUT)
+        {
+            tickCount_++;
+            if (isQuit_ == FALSE)
+            {
+                OnProcess();
+            }
+        }
+    }
+}
+
+void Thread::WorkerThread()
+{
+    while (!isQuit_)
+    {
+        OnProcess();
+    }
+}
+
+BOOL Thread::DestroyThread()
+{
+    isQuit_ = TRUE;
+    if (NULL != threadHandle_)
+    {
+        // work thread
+        if (waitTick_ == 0)
+        {
+            Thread::Stop();
+            Thread::Run();
+        }
+        // tick thread
+        else
+        {
+            if (isRun_ == FALSE)
+            {
+                Thread::Run();
+            }
+            SetEvent(quitEvent);
+        }
+        WaitForSingleObject(threadHandle_, INFINITE);
+    }
+    ReleaseResource();
+    return TRUE;
+}
+
+BOOL Thread::Run()
+{
+    if (isRun_ == FALSE)
+    {
+        isRun_ = TRUE;
+        ResumeThread(threadHandle_);
+        return TRUE;
+    }
+    return FALSE;
+}
+
+BOOL Thread::Stop()
+{
+    if (isRun_ == TRUE)
+    {
+        isRun_ = FALSE;
+        SuspendThread(threadHandle_);
+        return TRUE;
+    }
+    isQuit_ = TRUE;
+    return FALSE;
+}
+
+UINT32 Thread::GetTickCount()
+{
+    return tickCount_;
+}
+
+UINT32 Thread::GetWaitTick()
+{
+    return waitTick_;
+}
+
+BOOL Thread::IsRun()
+{
+    return isRun_;
+}
+
+BOOL Thread::IsQuit()
+{
+    return isQuit_;
+}
+
+void Thread::ReleaseResource()
+{
+    isQuit_ = TRUE;
+    isRun_ = FALSE;
+    //이벤트 리소스를 제거
+    if (quitEvent)
+    {
+        CloseHandle(quitEvent);
+        quitEvent = nullptr;
+    }
+
+    //쓰레드 리소스 제거
+    if (threadHandle_)
+    {
+        CloseHandle(threadHandle_);
+        threadHandle_ = nullptr;
+    }
+}
+
+UINT32 __stdcall Thread::CallTickThread(LPVOID p)
+{
+    Thread* tickThread = (Thread*)p;
+    if (tickThread->GetWaitTick())
+    {
+        tickThread->TickThread();
+    }
+    else
+    {
+        tickThread->WorkerThread();
+    }
+    return 1;
+}
---
+++ MyLogging/MyLogging/Thread.h
@@ -0,0 +1,37 @@
+﻿#ifndef THREAD
+#define THREAD
+
+#include<process.h>
+#include<windows.h>
+
+class Thread
+{
+public:
+    Thread();
+    virtual ~Thread();
+    BOOL CreateThread(UINT32 waitTick); // waitTick이 0이면 WorkerThread로 작동한다.
+    void TickThread(); // 일정 Tick마다 스케줄링 된다.
+    void WorkerThread(); // 계속 스케줄링 된다.
+    BOOL DestroyThread(); // 쓰레드를 파괴한다.
+    BOOL Run(); // 쓰레드를 시작한다.
+    BOOL Stop(); // 쓰레드를 멈춘다.
+    UINT32 GetTickCount();
+    UINT32 GetWaitTick();
+    BOOL IsRun();
+    BOOL IsQuit();
+    void ReleaseResource(); //할당된 자원을 풀어준다.
+    virtual void OnProcess() = 0; // 상속받은 클래스에서 처리될 일을 정의한다.
+
+protected:
+    HANDLE threadHandle_ = nullptr;
+    HANDLE quitEvent = nullptr;
+    UINT32 waitTick_ = 0;
+    UINT32 tickCount_ = 0;
+    BOOL isRun_ = FALSE;
+    BOOL isQuit_ = FALSE;
+
+private:
+    static UINT32 WINAPI CallTickThread(LPVOID p);
+};
+
+#endif // !THREAD
